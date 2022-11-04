c798179d7cb47227c2483c3b11dd47b7537c9bc3
systemsAnalysts <manjozib@gmail.com>
2022-11-04 14:20:59 +0200

    .h file

---
+++ sort.h
@@ -0,0 +1,14 @@
+#ifndef __SORT__H__
+#define __SORT__H__
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+
+
+void print_array(const int *array, size_t size);
+
+void bubble_sort(int *array, size_t size);
+
+void selection_sort(int *array, size_t size);
+
+#endif
