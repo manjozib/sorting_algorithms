4dd7bac511e274318844de229a38921ad9ea7099
systemsAnalysts <manjozib@gmail.com>
2022-11-08 15:18:40 +0200

    heap sort and its big O notation

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -1,72 +1,75 @@
 #include "sort.h"
 
 /**
- * heap_sort - Sorting array using heap sort algorithm
- * @array: Array to be sorted
+ * _swap - swaped 2 values.
+ * @array: the array for swap him values.
+ * @i: First index
+ * @j: Second index
+ * @r_size: The size constant for print
+ * Return: Nothing
+ */
+void _swap(int *array, int i, int j, const int r_size)
+{
+	int tmp;
+	(void) r_size;
+
+	if (i != j)
+	{
+		tmp = array[i];
+		array[i] = array[j];
+		array[j] = tmp;
+		print_array(array, (size_t)r_size);
+	}
+}
+
+/**
+ * _largest - Find the largest number btween the layers
+ * @array: The array for sort
+ * @size: The menor element
+ * @i: The largest.
+ * @r_size: The size for print in swap
+ * Return: Nothing.
+ */
+void _largest(int *array, size_t size, int i, const int r_size)
+{
+	int largest = i;
+	int lft = (2 * i) + 1;
+	int rgt = (2 * i) + 2;
+
+	if (lft < (int)size && array[lft] > array[largest])
+		largest = lft;
+
+	if (rgt < (int)size && array[rgt] > array[largest])
+		largest = rgt;
+
+	if (largest != i)
+	{
+		_swap(array, i, largest, r_size);
+		_largest(array, size, largest, r_size);
+	}
+}
+
+/**
+ * heap_sort - Call largest while exist layers
+ * @array: The array that generate the layers
  * @size: Size of the array
- * Return: 0
+ * Return: Nothing
  */
 void heap_sort(int *array, size_t size)
 {
+	const int r_size = (const int)size;
 	int i;
 
-	if (!array || size < 2)
+	if (size < 2 || !array)
 		return;
 
-	for (i = size / 2; i >= 0; i--)
-		heapify(array, size, i, size);
+	for (i = size / 2 - 1; i >= 0; i--)
+		_largest(array, size, i, r_size);
+
 	for (i = size - 1; i >= 0; i--)
 	{
-		swap(&array[i], &array[0]);
-		if (i != 0)
-			print_array(array, size);
-		heapify(array, i, 0, size);
+		_swap(array, 0, i, r_size);
+		_largest(array, i, 0, r_size);
 	}
 }
 
-/**
- * heapify - Recursive function to sort binary tree
- * @array: array to be sorted as binary tree
- * @end: Last node in binary tree
- * @start: First node of binary tree
- * @size: Size of the array to sort
- * Return: 0
- */
-void heapify(int *array, int end, int start, size_t size)
-{
-	int max = start;
-	int left = 2 * start + 1;
-	int right = 2 * start + 2;
-
-	if (!array || size < 2)
-		return;
-
-	if (left < end && array[left] > array[max])
-		max = left;
-
-	if (right < end && array[right] > array[max])
-		max = right;
-
-	if (start != max)
-	{
-		swap(&array[start], &array[max]);
-		print_array(array, size);
-		heapify(array, end, max, size);
-	}
-}
-
-/**
- * swap - Function that swaps two values
- *
- * @a: Fisrt value
- * @b: Second value
- * Return: 0
- */
-void swap(int *a, int *b)
-{
-	int tmp;
-
-	tmp = *b;
-	*b = *a;
-	*a = tmp;
-}
