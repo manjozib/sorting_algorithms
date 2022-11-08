f0659a206840f1c0e6bc7e0899b0d29630fd6271
systemsAnalysts <manjozib@gmail.com>
2022-11-08 14:15:40 +0200

    changes in .h file

--- sort.h
+++ sort.h
@@ -23,7 +23,6 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void quick_sort(int *array, size_t size);
 void selection_sort(int *array, size_t size);
-size_t partition(int array[], size_t size);
 void print_sort(int array[], size_t size, int init);
 
 /* advanced tasks prototypes */
